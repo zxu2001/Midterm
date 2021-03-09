dd592a04819140b50161bb437907835b29c60ede
Tommy Xu <zwxu@umich.edu>
2021-03-09 08:21:12 -0500

    20+30

--- AddIntegers.java
+++ AddIntegers.java
@@ -2,8 +2,8 @@ public class AddIntegers {
 
     public static void main(String[] args) {
 
-        int first = 10;
-        int second = 20;
+        int first = 20;
+        int second = 30;
 
         System.out.println("Enter two numbers: " + first + " " + second);
         int sum = first + second;
